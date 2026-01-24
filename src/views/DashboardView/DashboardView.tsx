18406e497b1a5b7353e19a56c961aa65f92f4168
Finn Freitag <finn-freitag@web.de>
2026-01-25 00:16:08 +0100

    fix: auto logout

--- src/views/DashboardView/DashboardView.tsx
+++ src/views/DashboardView/DashboardView.tsx
@@ -32,7 +32,7 @@ export default function DashboardView(props: ViewProps) {
                 });
         }, 20000);
         return () => clearInterval(interval);
-    },[]);
+    },[dashboardViewProps]);
 
     function logout() {
         fetch("/api/logout", {method: "POST",headers: {"Content-Type": "application/json"}, body: JSON.stringify({sessionToken: dashboardViewProps?.sessionToken})})
