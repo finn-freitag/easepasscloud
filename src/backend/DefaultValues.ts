476cb96c08eddb9317e504011900578d97ad1257
Finn Freitag <finn-freitag@web.de>
2026-01-29 19:43:12 +0100

    feat: default values using environment variables #7

--- src/backend/DefaultValues.ts
+++ src/backend/DefaultValues.ts
@@ -1,4 +1,4 @@
-export const DefaultSessionTimeoutHours = 3;
-export const DefaultAccessTokenExpiryDays = 30;
-export const DefaultAutoUnlockTimeoutMinutes = 60;
-export const DefaultViewUpdateTime = 5000;
+export const DefaultSessionTimeoutHours = process.env.DEFAULT_SESSION_TIMEOUT_HOURS ?? 3;
+export const DefaultAccessTokenExpiryDays = process.env.DEFAULT_ACCESS_TOKEN_EXPIRY_DAYS ?? 30;
+export const DefaultAutoUnlockTimeoutMinutes = process.env.DEFAULT_AUTO_UNLOCK_TIMEOUT_MINUTES ?? 60;
+export const DefaultViewUpdateTime = process.env.DEFAULT_VIEW_UPDATE_TIME ?? 5000;
